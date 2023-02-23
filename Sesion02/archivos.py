95bac69771fae500f797f416f1a55b9044f92822
efccc0010 <edgarfernandocarrionccoicca@gmail.com>
2023-02-22 22:23:33 -0500

    Add files

--- Sesion02/archivos.py
+++ Sesion02/archivos.py
@@ -10,3 +10,39 @@ f.write("\nManzana,5,10")
 
 f = open("archivo2.txt","w")
 f.write("HOLA MUNDO")
+
+
+# cuestionario 3 pregunta 2
+def mostrarPixeles():
+    file = open("pixeles.txt")
+    pixeles = []
+    for line in file:
+        line = line.strip()
+        line = line.split(",")
+        pixeles.append(line[0])
+    print(pixeles)
+ 
+def procesarPixeles():
+    file = open("pixeles.txt")
+    pixeles = []
+    for line in file:
+        line = line.strip()
+        line = line.split(",")
+        pixeles.append(line[0])
+    total = sum(pixeles)/len(pixeles)
+    return total
+ 
+def comprimirPixeles():
+    file = open("pixeles.txt")
+    pixeles = []
+    for line in file:
+        line = line.strip()
+        line = line.split(",")
+        pixeles.append(line[0])
+    comprimido = rle(pixeles)
+    return comprimido
+ 
+def rle(pixeles):
+    comprimido = []
+    # ... procesamiento ...
+    return comprimido
